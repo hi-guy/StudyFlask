c432e0e9c710ad5931e4f6c01bb0f19e30172c90
hi-guy <zhouyi.guy@gmail.com>
2018-04-25 18:02:44 +0800

    blueprint

--- microblog/app/models.py
+++ microblog/app/models.py
@@ -1,10 +1,12 @@
-from app import db, login, app
+from app import db, login
 from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from hashlib import md5
 from time import time
 import jwt
+from flask import current_app
+
 
 followers = db.Table('followers',
                      db.Column('follower_id', db.Integer,
@@ -68,16 +70,17 @@ class User(UserMixin, db.Model):
                 'reset_password': self.id,
                 'exp': time() + expires_in
             },
-            app.config['SECRET_KEY'],
+            current_app.config['SECRET_KEY'],
             algorithm='HS256').decode('utf-8')
 
     @staticmethod
     def verify_reset_password_token(token):
         try:
-            id = jwt.decode(token, app.config['SECRET_KEY'], algorithms=['HS256'])['reset_password']
+            id = jwt.decode(token, current_app.config['SECRET_KEY'], algorithms=['HS256'])['reset_password']
         except:
             return
         return User.query.get(id)
+
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
--- microblog/config.py
+++ microblog/config.py
@@ -1,5 +1,9 @@
 import os
+from dotenv import load_dotenv
+
+
 basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, '.env'))
 
 
 class Config(object):
@@ -7,16 +11,15 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    MAIL_DEBUG = os.environ.get('MAIL_DEBUG')
-    MAIL_SUPPRESS_SEND = os.environ.get('MAIL_SUPPRESS_SEND')
-    MAIL_SERVER = os.environ.get('MAIL_SERVER')
-    MAIL_PORT = os.environ.get('MAIL_PORT')
-    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS')
-    MAIL_USE_SSL = os.environ.get('MAIL_USE_SSL')         
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    MAIL_DEFAULT_SENDER = os.environ.get('MAIL_DEFAULT_SENDER')
+    MAIL_DEBUG = os.getenv('MAIL_DEBUG')
+    MAIL_SUPPRESS_SEND = False
+    MAIL_SERVER = os.getenv('MAIL_SERVER')
+    MAIL_PORT = os.getenv('MAIL_PORT')
+    MAIL_USE_TLS = False
+    MAIL_USE_SSL = True         
+    MAIL_USERNAME = os.getenv('MAIL_USERNAME')
+    MAIL_PASSWORD = os.getenv('MAIL_PASSWORD')
+    MAIL_DEFAULT_SENDER = os.getenv('MAIL_DEFAULT_SENDER')
     POSTS_PER_PAGE = 20
     LANGUAGES = ['en', 'es']
-    MS_TRANSLATOR_KEY = os.environ.get('MS_TRANSLATOR_KEY')
-
+    MS_TRANSLATOR_KEY = os.getenv('MS_TRANSLATOR_KEY')
